586749a9bcfc8a581725a1d2acb39bf7688b6512
Salvador Mulfari <josesalvador0102@gmail.com>
2023-04-02 21:03:18 -0400

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,16 @@ import Fortuna from './components/Fortuna'
 import Espacio from './components/espacio'
 
 function App() {
-
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Generador de frases</h1>
-      </header>
-      <main className="App-main">
-        <div className="component-row">
-          <div className="component-container">
-            <Celebres />
-          </div>
-          <div className="component-container">
-            <Espacio />
-          </div>
-          <div className="component-container">
-            <Fortuna />
-          </div>
-        </div>
-      </main>
+      <div className="components-container">
+        <Celebres />
+        <Espacio />
+        <Fortuna />
+      </div>
     </div>
-  );  
+  );
 }
+
 
 export default App
