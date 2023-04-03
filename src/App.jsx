8d9fa25a17ab7a29181420b2bb351554583d840a
Salvador Mulfari <josesalvador0102@gmail.com>
2023-04-03 16:41:54 -0400

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import './App.css'
 function App() {
   return (
     <div className="App">
-      <h1 className="title">Generador de Frases</h1>
+      <h1 className="title1">Generador de Frases</h1>
       <div className="components-container">
         <Celebres />
         <Espacio />
