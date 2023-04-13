64aa8e8dfac3623d4acd91be16b75704160b372b
Salvador Mulfari <josesalvador0102@gmail.com>
2023-04-13 14:02:42 -0400

    asd

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './styles/Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="light"></div>
+      <h1 className="header-title">Cosmic Curiosities</h1>
+    </header>
+  );
+};
+
+export default Header;
